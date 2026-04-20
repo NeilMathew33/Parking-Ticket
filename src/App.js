3ac4c629cde23fbcf4e16360ceb289bcb469cb4d
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-19 20:36:35 -0700

    added signup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddParkingTicket from './Components/AddParkingTicket';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ViewAllParkingTickets from './Components/ViewAllParkingTickets';
 import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 <Route path="/Add" element = {<AddParkingTicket />} />
 <Route path="/ViewAll" element = {<ViewAllParkingTickets />} />
 <Route path="/" element = {<Login />} />
+<Route path="/SignUp" element = {<SignUp />} />
     </Routes>
     </BrowserRouter>
   );
